03e97119b4bf31ab9a45be78a6b1941be1433661
Munir-Hassan <xvidplasma@gmail.com>
2020-08-20 22:10:44 +0600

    Updates

---
+++ precache-manifest.2ea00a36a438e80681d83a2c81891eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70536bd5c86c3784711130459785164",
+    "url": "/Munir-Hassan/imam-trust/index.html"
+  },
+  {
+    "revision": "9fb601d869916d981cf4",
+    "url": "/Munir-Hassan/imam-trust/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c2d681ec39730adc8a11",
+    "url": "/Munir-Hassan/imam-trust/static/css/main.6d2256b2.chunk.css"
+  },
+  {
+    "revision": "9fb601d869916d981cf4",
+    "url": "/Munir-Hassan/imam-trust/static/js/2.876b6481.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Munir-Hassan/imam-trust/static/js/2.876b6481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d681ec39730adc8a11",
+    "url": "/Munir-Hassan/imam-trust/static/js/main.ecce8a5b.chunk.js"
+  },
+  {
+    "revision": "c2e945859c4a36858bab",
+    "url": "/Munir-Hassan/imam-trust/static/js/runtime-main.ce935428.js"
+  }
+]);
