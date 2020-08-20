ca43e27fd4f732035e7ab22bc4eed3cf678d7c23
Munir-Hassan <xvidplasma@gmail.com>
2020-08-20 22:03:03 +0600

    creating a frontend components

---
+++ src/components/SignInForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SignInForm = () => {
+	return (
+		<div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam sed cum deserunt reiciendis repudiandae
+				nostrum optio vel iure dolore incidunt voluptate, mollitia facilis accusantium quia dicta? Laudantium
+				minus velit a!
+			</p>
+		</div>
+	);
+};
+
+export default SignInForm;
